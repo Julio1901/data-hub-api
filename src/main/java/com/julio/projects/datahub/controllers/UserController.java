323ca3f060a5f3c2d239263e98a707c811fe5828
Julio Cesar <js.cesar42@gmail.com>
2024-02-24 21:58:36 -0300

    tech: swagger end points documented

--- src/main/java/com/julio/projects/datahub/controllers/UserController.java
+++ src/main/java/com/julio/projects/datahub/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.julio.projects.datahub.controllers;
 import com.julio.projects.datahub.dtos.UserDto;
 import com.julio.projects.datahub.models.UserModel;
 import com.julio.projects.datahub.repositories.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +17,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Users")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Operation(
+            description = "Post endpoint for create user",
+            summary = "This is the summary for create user",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User created successfully"),
+                    @ApiResponse(responseCode = "400", description = "Failed to create user"),
+                    @ApiResponse(responseCode = "409", description = "User already exists")
+            }
+    )
 
     @PostMapping("/users")
     public ResponseEntity<Object> saveUser(@RequestBody @Valid UserDto userDto){
--- src/main/java/com/julio/projects/datahub/controllers/login/LoginController.java
+++ src/main/java/com/julio/projects/datahub/controllers/login/LoginController.java
@@ -2,6 +2,9 @@ package com.julio.projects.datahub.controllers.login;
 
 import com.julio.projects.datahub.dtos.UserDto;
 import com.julio.projects.datahub.services.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +13,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Login")
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
+    @Operation(
+            description = "Post endpoint to make login",
+            summary = "This is the summary to make login",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Login successfully"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            }
+    )
     @PostMapping("/login")
     public ResponseEntity<Object> getUser(@RequestBody UserDto loginRequest) {
         if(loginService.authenticate(loginRequest.userName(), loginRequest.password())){
